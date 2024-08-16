dff92395c3a04292091de5b7cc829d1552ea6cee
SergeKryv <krivosheevsrg@gmail.com>
2024-08-16 20:47:37 +0200

    WN8 chart styling improved

--- player_portal/portal_web/static/portal_web/js/home_utils.js
+++ player_portal/portal_web/static/portal_web/js/home_utils.js
@@ -34,7 +34,10 @@ new Chart("leftChart", {
     datasets: [{
         backgroundColor:"rgba(0,0,255,1.0)",
         borderColor: "rgba(0,0,255,0.1)",
-        data: values
+        data: values,
+        showLine: true,
+        fill: false,
+        tension: 0.3
     }]
   },
   options:{
